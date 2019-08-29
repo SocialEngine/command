5d2ce59cea5fe3c628229ad42456f0c9c70fff7f
RaymondBenc <me@raybenc.com>
2019-08-29 15:35:51 +0200

    Remove config on reset.

--- src/commands/reset.js
+++ src/commands/reset.js
@@ -5,17 +5,22 @@ const fs = require('fs');
 const dir = require('../app/dir');
 
 commander.command('reset').action(() => {
-    const toDelete = ['/.se', '/src', '/srv'];
+    const toDelete = ['/.se', '/src', '/srv', '/.cache/config.json'];
     const answer = readline.question('Are you sure? [y/n]: ');
     if (typeof answer === 'string' && answer === 'y') {
         console.log('Resetting:', toDelete);
         for (const item of toDelete) {
             const absolutePath = path.join(process.cwd(), item);
             if (fs.existsSync(absolutePath)) {
-                dir.delete(absolutePath);
+                const stat = fs.statSync(absolutePath);
+                if (stat.isDirectory()) {
+                    dir.delete(absolutePath);
+                    fs.mkdirSync(absolutePath);
+                    fs.writeFileSync(path.join(absolutePath, '/.gitkeep'), '', 'utf-8');
+                } else {
+                    fs.unlinkSync(absolutePath);
+                }
             }
-            fs.mkdirSync(absolutePath);
-            fs.writeFileSync(path.join(absolutePath, '/.gitkeep'), '', 'utf-8');
         }
     }
 });
